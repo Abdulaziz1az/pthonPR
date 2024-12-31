c61624c17fe44f2dd8b5eda9e7800d85a0f4267e
Abdulaziz <AbdulazizAbdirisak15@gmail.com>
2024-12-30 23:36:58 -0600

    I declar the global constant

---
+++ birthdays.py
@@ -0,0 +1,13 @@
+# This program use dictinary to keep friends names and birthdays
+# Global constant
+LOOK_UP = 1
+ADD = 2
+CHANGE = 3
+DELETE = 4
+QUIT = 5
+
+# main function
+def main():
+    # empty dictionary.
+    birthdays = {}
+    
