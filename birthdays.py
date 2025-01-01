9c275980b03d3d87dd642a7a56a627999a3b88e2
Abdulaziz <AbdulazizAbdirisak15@gmail.com>
2025-01-01 08:17:45 -0600

    menu choice from the user

--- birthdays.py
+++ birthdays.py
@@ -22,3 +22,23 @@ def main():
             change(birthdays)
         elif choice == DELETE:
             delete(birthdays)
+
+# get menu choice from the user
+def get_menu_choice():
+    print()
+    print("Friends and Their Birthdays")
+    print("---------------------------")
+    print("1. Look up a birthday")
+    print("2. Adda new birthday")
+    print("3. Change a birthday")
+    print("4. Delete a birthday")
+    print("5. Quite the program")
+    print()
+
+    # get the user choice
+    choice = int(input("Enter your choice : "))
+
+    # Validate the choice
+    while choice < LOOK_UP or choice > QUIT:
+        choice = int (input("Enter a valid choice: "))
+    return choice
