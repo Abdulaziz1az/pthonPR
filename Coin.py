9849d6c04d4e71152105a837a0600a9b00cc4b18
Abdulaziz <AbdulazizAbdirisak15@gmail.com>
2025-01-09 10:22:14 -0600

    I added getsideup method and main method it needs some debuging it only displays heads evey time

--- Coin.py
+++ Coin.py
@@ -2,13 +2,29 @@ import random
 # The Coin class simulates a coin that ca n be flipped
 class Coin:
     def __init__(self):
-        self.___sideup = "Head"
+        self.sideup = "Head"
 
     # The toss method generates a random number
     # in the range of 0 through 1 if the number is 0 then sid is set to Head
     # otherwise, sideup is set ot "Tails"
     def toss(self):
-        if random.radnint(0,1) == 0:
-            self.___sideup = "Head"
+        if random.randint(0,1) == 0:
+            self.sideup = "Heads"
         else:
-            self.___sideup = "Tails"
+            self.sideup = "Tails"
+    # returns the value
+    def get_sidup(self):
+        return self.sideup
+# The main function
+def main():
+    # object  from the Coin class
+    my_coin = Coin()
+
+    # display the side of the coin that is facing up 
+    print("This side is up :", my_coin.get_sidup())
+    # Toss the coin 
+    print("I am tossing the coin...")
+    # display the side fo the coin that is facing up
+    print("This side is up: ", my_coin.get_sidup())
+# Call the function 
+main()
